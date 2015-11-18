d13d284cfc4b96a29bf184785853521b033dd608
tech1arb <tech1arb@gmail.com>
2015-11-18 06:48:22 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -15,12 +15,80 @@ NOTES:
 
 #include <iostream>
 #include <malloc.h>
-
-struct student {
-	char *name;
+struct student
+{
+	char name[10];
 	int score;
 };
 
-struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+void copy(struct student *a, struct student **b, int i, int j)
+{
+	//printf("%c", a[0]);
+	int k = 0;
+
+	for (k = 0; k<10; k++)
+	{
+		//printf("h");
+		b[j]->name[k] = a[i].name[k];
+		//printf("%c",a[i]);
+	}
+}
+void sortstu(int m, struct student  *x)
+{
+	int i = 0, j = 0, t;
+	for (i = 0; i <=m - 1; i++)
+		for (j = 0; j <m - i-1; j++)
+			if (x[j].score >= x[j + 1].score)
+			{
+				t = x[j].score;
+				x[j].score = x[j + 1].score;
+				x[j + 1].score = t;
+				char	*str1 = x[j].name;
+				*x[j].name = *x[j + 1].name;
+				*x[j + 1].name = *str1;
+			}
+}
+struct student ** topKStudents(struct student *students, int len, int K)
+{
+	int i, j = 0, len1 = len - K;
+	struct student **arr = NULL;
+	arr = (struct student **)realloc(arr, (K + 1) * sizeof(struct studnet *));
+
+	//arr = (struct student*)malloc(sizeof(struct student*)*K);
+
+
+	if (students[0].name == NULL)
+		return NULL;
+	if (len < 0)
+		return NULL;
+	if (K == 0)
+		return NULL;
+	for (i = 0; i < len; i++)
+	{
+
+		if (students[i].score == NULL)
+		{
+			return NULL;
+			break;
+		}
+	}
+
+	sortstu(len, students);
+	if (len1 < 0)
+	{
+		len1 = 0;
+	}
+	for (i = len1; i < len; i++)
+	{
+		arr[j] = (struct student *)malloc(sizeof(struct student));
+
+		//arr[j] = (struct student*)malloc(sizeof(struct student));
+		//arr[j]->name = (char *)malloc(sizeof(char)*10);
+		copy(students, arr, i, j);
+		arr[j]->score = students[i].score;
+		//printf("%d", arr[j]->score);
+		//printf("%s\n", arr[j]->name);
+		j++;
+	}
+	return arr;
 }
